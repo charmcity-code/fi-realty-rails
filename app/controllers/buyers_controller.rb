32ec1178463ae70d7d215b7873ef77d64b6613d9
Mark Lawrence <lawrence.ml@gmail.com>
2018-12-09 14:32:19 -0500

    created index, new and show methods and views

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,2 +1,30 @@
 class BuyersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @buyer = Buyer.new
+  end
+
+  def create
+    @buyer = Buyer.new(buyer_params)
+
+    if @buyer.save
+      redirect_to buyer_path(@buyer)
+    else
+      render new_buyer_path
+    end
+  end
+
+  def show
+    @buyer = Buyer.find_by(id: params[:id])
+  end
+
+  private
+
+  def buyer_params
+    params.require(:buyer).permit(:first_name, :last_name, :email)
+  end
 end
