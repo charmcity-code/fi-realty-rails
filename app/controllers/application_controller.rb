73ce068b2f4ea11e3986c0d43482381c039a0fe3
Mark Lawrence <lawrence.ml@gmail.com>
2018-12-12 16:43:52 -0500

    added listing controller methods and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def chronic_time(string)
+    Chronic.parse(string)
+  end
+
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,31 @@
 class ListingsController < ApplicationController
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    # binding.pry
+
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render new_listing_path
+    end
+  end
+
+  def index
+    @listings = Listing.all
+  end
+
+  def show
+    @listing = Listing.find_by(id: params[:id])
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:street, :city, :state, :zip_code, :property_type, :bedrooms, :bathrooms, :list_price, :list_date, :user_id)
+  end
 end
