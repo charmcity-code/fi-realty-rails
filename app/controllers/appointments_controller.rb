9ee56b69d2590b0250dc40a1ccea747597a06cb3
Mark Lawrence <lawrence.ml@gmail.com>
2018-12-13 22:25:13 -0500

    initial build of buyers index

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,26 @@
 class AppointmentsController < ApplicationController
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+
+    if @appointment.save
+      redirect_to appointment_path(@appointment)
+    else
+      render new_appointment_path
+    end
+  end
+
+  def show
+    @appointment = Appointment.find_by(id: params[:id])
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:buyer_name, :listing_street, :date)
+  end
 end
