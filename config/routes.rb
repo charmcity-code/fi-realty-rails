9ff499335ec9c3a5f1f20d1f06966dae7c7cb280
Mark Lawrence <lawrence.ml@gmail.com>
2018-12-19 17:08:29 -0500

    added correct routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Rails.application.routes.draw do
+
+  root 'static#welcome'
+
   resources :listings
   resources :users
+  # no user index
 
   resources :buyers do
     resources :appointments, only: [:index, :new]
   end
 
   resources :appointments
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   get '/login' => 'sessions#new'
-  post '/session' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
-  root 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  delete '/logout' => 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
