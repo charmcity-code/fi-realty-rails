6637b2673ea5cdeb06f54fb8e2ea0b513c4c8956
Mark Lawrence <lawrence.ml@gmail.com>
2018-12-22 01:12:21 -0500

    created logic to log in with GitHub OAuth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:user][:username])
-
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    if auth_hash = request.env["omniauth.auth"]
+     @user = User.find_or_create_by_omniauth(auth_hash)
+     session[:user_id] = @user.id
+     redirect_to user_path(@user)
     else
-      flash[:message] = "Oops! Something went wrong."
-      redirect_to login_path
+      @user = User.find_by(email: params[:user][:email])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        flash[:message] = "Oops! Something went wrong."
+        redirect_to login_path
+      end
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   has_many :appointments, through: :listings, source: :user
 
   validates :email, presence: true, uniqueness: { case_sensitive: :false }
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
+      user.password = SecureRandom.hex
+    end
+  end
+
 end
